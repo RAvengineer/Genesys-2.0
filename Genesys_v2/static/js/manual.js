e856d71176ee743d9e71be4642e6a62a787298d9
RAvengineer <bera2591@gmail.com>
2019-11-21 23:15:10 +0530

    Auto-update Base Motors Status with a Static Value

--- Genesys_v2/static/js/manual.js
+++ Genesys_v2/static/js/manual.js
@@ -1,1 +1,17 @@
-var data_refresh_interval = 1000; // in (ms)
+setInterval(getGamepadKeys,data_refresh_interval);
+
+function getGamepadKeys(){
+    $.ajax({
+        url: "/getGamepadKeys",
+        success: updateRoverMotorsStatus,
+        fail: function(){
+            console.log("Get Gamepad Keys in manual.js FAILED");
+        }
+    });
+}
+
+function updateRoverMotorsStatus(data){
+    if(data.mode==="baseMotors"){
+        $("#BaseStatus").text(data.command)
+    }
+}
