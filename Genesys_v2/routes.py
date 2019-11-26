a808e2b66600138e1b2edf688b48ae510c3b9371
RAvengineer <bera2591@gmail.com>
2019-11-26 14:16:11 +0530

    Remove Redundant code

--- Genesys_v2/routes.py
+++ Genesys_v2/routes.py
@@ -121,33 +121,6 @@ TODO 6: Fail Safe for Gamepad
         Also, add a code for controlling the rover through keyboard
 """
 
-# Read for understanding the return type of request.form: https://werkzeug.palletsprojects.com/en/0.15.x/datastructures/
-@app.route('/upload', methods=['POST'])
-def upload():
-    if request.method == "POST":
-        # Request the forms to return the inputs in form of dictionary
-        pins_dict = request.form.to_dict() # Output: dict_keys(['dp0', 'dp3', 'dp6', 'ap0', 'ap4'])
-        print(pins_dict)
-
-        # Create an empty list for digital pins
-        digital_pins = []
-        # Create an empty list for analog pins
-        analog_pins = []
-
-        # print(pins_dict.keys()) # Debugging
-
-        # Append the pin numbers in their appropriate lists
-        for pin in list(pins_dict.keys()):
-            if('dp' in pin):
-                digital_pins.append(int(pin[2:]))
-            elif('ap' in pin):
-                analog_pins.append(int(pin[2:]))
-        
-        # Debugging
-        # print(digital_pins)
-        # print(analog_pins)
-    return render_template('upload.html')
-
 
 @app.route('/user/<username>')
 def profile(username):
