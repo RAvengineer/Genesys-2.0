4868a8af69eb8d216941ba31a712ea8e3511ee06
RAvengineer <bera2591@gmail.com>
2019-11-28 22:16:22 +0530

    Remove redundant code and add getCodeWord method

--- Utilities/GamepadControls.py
+++ Utilities/GamepadControls.py
@@ -1,7 +1,60 @@
+from Utilities.GenerateCodeWord import GenerateCodeword
+gcw = GenerateCodeword()
+commandsCodeWord = {
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    "command": gcw.parseBase("F"),
+    "STOP command": gcw.parseBase("S"),
+    
+}
+
 class GamepadControls:
     """
-    A Class to get Inputs using a Gamepad
+    A Class to convert the commands received from the front-end\n
+    via the Gamepad into code word to be sent to the Rover
     """
     def __init__(self):
         pass
 
+
+    def getCodeWord(command):
+        try:
+            return commandsCodeWord(command)
+        except:
+            print("Invalid Command in getCodeWord in GamepadControls.py")
