deeb318e738bb25e232f38c927363c72f3e5621b
RAvengineer <bera2591@gmail.com>
2019-11-28 23:47:28 +0530

    Dictionary for mapping all commands via gamepad

--- Utilities/GamepadControls.py
+++ Utilities/GamepadControls.py
@@ -1,6 +1,7 @@
 from Utilities.GenerateCodeWord import GenerateCodeword
 gcw = GenerateCodeword()
 commandsCodeWord = {
+    # Base
     "Forward": gcw.parseBase("F"),
     "STOP Forward": gcw.parseBase("S"),
     "Backward": gcw.parseBase("B"),
@@ -9,40 +10,51 @@ commandsCodeWord = {
     "STOP Left": gcw.parseBase("S"),
     "Right": gcw.parseBase("R"),
     "STOP Right": gcw.parseBase("S"),
-    "0": gcw.parseCamera(1),
+    # Camera Number
+    "0": gcw.parseCamera(0),
     "1": gcw.parseCamera(1),
     "2": gcw.parseCamera(2),
     "3": gcw.parseCamera(3),
     "4": gcw.parseCamera(4),
+    # ARM Base
     "ARM Base Left": gcw.parseArm("L"),
     "STOP ARM Base Left": gcw.parseArm("S"),
     "ARM Base Right": gcw.parseArm("R"),
     "STOP ARM Base Right": gcw.parseArm("S"),
-    "command": gcw.parseActuator1("E"),
-    "STOP command": gcw.parseActuator1("S"),
-    "command": gcw.parseActuator1("R"),
-    "STOP command": gcw.parseActuator1("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    
+    # Actuator 1
+    "Actuator 1 EXTEND": gcw.parseActuator1("E"),
+    "STOP Actuator 1 EXTEND": gcw.parseActuator1("S"),
+    "Actuator 1 RETRACT": gcw.parseActuator1("R"),
+    "STOP Actuator 1 RETRACT": gcw.parseActuator1("S"),
+    # Actuator 2
+    "Actuator 2 EXTEND": gcw.parseActuator2("E"),
+    "STOP Actuator 2 EXTEND": gcw.parseActuator2("S"),
+    "Actuator 2 RETRACT": gcw.parseActuator2("R"),
+    "STOP Actuator 2 RETRACT": gcw.parseActuator2("S"),
+    # Actuator 3
+    "Actuator 3 EXTEND": gcw.parseActuator3("E"),
+    "STOP Actuator 3 EXTEND": gcw.parseActuator3("S"),
+    "Actuator 3 RETRACT": gcw.parseActuator3("R"),
+    "STOP Actuator 3 RETRACT": gcw.parseActuator3("S"),
+    #  Gripper Direction
+    "Gripper Rotate LEFT": gcw.parseGripperDir("L"),
+    "STOP Gripper Rotate LEFT": gcw.parseGripperDir("S"),
+    "Gripper Rotate RIGHT": gcw.parseGripperDir("R"),
+    "STOP Gripper Rotate RIGHT": gcw.parseGripperDir("S"),
+    # Gripper State
+    "Gripper OPEN": gcw.parseGripperState("O"),
+    "STOP Gripper OPEN": gcw.parseGripperState("S"),
+    "Gripper CLOSE": gcw.parseGripperState("C"),
+    "STOP Gripper CLOSE": gcw.parseGripperState("S"),
+    # Modify PWM
+    "Increase PWM": gcw.parsePWM("U"),
+    "STOP Increase PWM": gcw.parsePWM("S"),
+    "Decrease PWM": gcw.parsePWM("D"),
+    "STOP Decrease PWM": gcw.parsePWM("S"),
+    # Random
+    "Null":-1,
+    "STOP gOF":-1,
+    "STOP tm":-1,    
 }
 
 class GamepadControls:
