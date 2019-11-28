d62b625a6df12ef98ecc486e100047d5454e314a
RAvengineer <bera2591@gmail.com>
2019-11-28 23:07:15 +0530

    Fix Nomenclature of the commands

--- Utilities/GamepadControls.py
+++ Utilities/GamepadControls.py
@@ -1,26 +1,27 @@
 from Utilities.GenerateCodeWord import GenerateCodeword
 gcw = GenerateCodeword()
 commandsCodeWord = {
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
-    "command": gcw.parseBase("F"),
-    "STOP command": gcw.parseBase("S"),
+    "Forward": gcw.parseBase("F"),
+    "STOP Forward": gcw.parseBase("S"),
+    "Backward": gcw.parseBase("B"),
+    "STOP Backward": gcw.parseBase("S"),
+    "Left": gcw.parseBase("L"),
+    "STOP Left": gcw.parseBase("S"),
+    "Right": gcw.parseBase("R"),
+    "STOP Right": gcw.parseBase("S"),
+    "0": gcw.parseCamera(1),
+    "1": gcw.parseCamera(1),
+    "2": gcw.parseCamera(2),
+    "3": gcw.parseCamera(3),
+    "4": gcw.parseCamera(4),
+    "ARM Base Left": gcw.parseArm("L"),
+    "STOP ARM Base Left": gcw.parseArm("S"),
+    "ARM Base Right": gcw.parseArm("R"),
+    "STOP ARM Base Right": gcw.parseArm("S"),
+    "command": gcw.parseActuator1("E"),
+    "STOP command": gcw.parseActuator1("S"),
+    "command": gcw.parseActuator1("R"),
+    "STOP command": gcw.parseActuator1("S"),
     "command": gcw.parseBase("F"),
     "STOP command": gcw.parseBase("S"),
     "command": gcw.parseBase("F"),
@@ -53,8 +54,8 @@ class GamepadControls:
         pass
 
 
-    def getCodeWord(command):
+    def getCodeWord(self,command):
         try:
-            return commandsCodeWord(command)
+            return commandsCodeWord[command]
         except:
             print("Invalid Command in getCodeWord in GamepadControls.py")
