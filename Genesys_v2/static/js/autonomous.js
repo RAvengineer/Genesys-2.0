a7fb87b0e58a80975c319efd5d44fc81886f80da
RAvengineer <bera2591@gmail.com>
2019-11-29 16:37:40 +0530

    Add feature: Send GPS from front-end to back-end

--- Genesys_v2/static/js/autonomous.js
+++ Genesys_v2/static/js/autonomous.js
@@ -1,3 +1,23 @@
+function updateLocationList(data){
+
+}
+
 $("#btnAddGPS").on("click",function () {
-    console.log("Add");
+    data_to_be_sent = {
+        "addGpsLat":$("#addGpsLat").val(),
+        "addGpsLon":$("#addGpsLon").val(),
+    }
+    $.ajax({
+        type: "POST",
+        url: "/addGPS",
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify(data_to_be_sent),
+        success:updateLocationList,
+        fail: function(){
+            console.log("Add GPS co-ordinates in autonomous.js failed");
+        }
+    });
+    $("#addGpsLat").val("");
+    $("#addGpsLon").val("");
 });
