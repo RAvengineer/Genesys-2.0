1d8c0f15097c2ee112450c4dc4ee7e4e5e5c3019
RAvengineer <bera2591@gmail.com>
2019-11-29 15:58:00 +0530

    Add Feature: Get values for Enabled Soil sensors

--- Genesys_v2/static/js/science.js
+++ Genesys_v2/static/js/science.js
@@ -1,8 +1,26 @@
+function getSoilChecked() {
+    return [
+    $("#swtatmPressure").is(':checked'),
+    $("#swtatmTemp").is(':checked'),
+    $("#swtatmHum").is(':checked'),
+    $("#swtCH4").is(':checked'),
+    $("#swtUV").is(':checked'),
+    $("#swtsoilTemp").is(':checked'),
+    $("#swtsoilpH").is(':checked'),
+    $("#swtsoilMoisture").is(':checked'),
+    ];
+}
+
 setInterval(getSensorValues,data_refresh_interval);
 
 function getSensorValues() {
+    data_to_be_sent = {'SoilSensorsChecked':getSoilChecked()};
     $.ajax({
+        type: "POST",
         url: "/getSensorValues",
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify(data_to_be_sent),
         success:updateSensorValues,
         fail: function(){
             console.log("Get Sensor Values in science.js failed");
@@ -20,5 +38,4 @@ function updateSensorValues(data) {
     $("#soilTemp").text(data.soilTemp);
     $("#soilpH").text(data.soilpH);
     $("#soilMoisture").text(data.soilMoisture);
-    $("#motor6").text(data.motor6);
 }
